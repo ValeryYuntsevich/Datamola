46738858ee3f976cdc9f06b27f2a78403a5a7b6b
valery <valery.yuntsevich@yandex.ru>
2020-03-30 21:48:46 +0300

    release v1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SerialsComponent } from './serials/serials.component';
 
+import { CoreRoutingModule } from './core-routing.module';
+import { AppMaterialModule } from '../app-material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { SerialsComponent } from './serials/serials.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent, SerialsComponent],
+  declarations: [
+    SerialsComponent, 
+    PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppMaterialModule
   ]
 })
 export class CoreModule { }
